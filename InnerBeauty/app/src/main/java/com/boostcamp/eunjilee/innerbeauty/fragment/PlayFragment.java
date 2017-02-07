ee38809627500efc9849f147c6a94b78e0bef811
eunjilee <tkl0458@gmail.com>
2017-02-07 21:28:36 +0900

    #3 InnerBeauty프로젝트를 생성 및 test파일들 제거 완료 #4 User, Play, Exhibition 모델클래스 작성 #5 TabLayout, NavigationDrawer, ViewPager적용하여 UI 템플릿 작성 #6 Retrofit을 이용하여 서버에서 값을 앱으로 가져오는거 까지 확인 완료. (; RxJava스터디 필요) #7 AWS서버 개설(ubuntu) / Exhibition,Play,User db생성(mysql) / Exhibition와Play 정보를 가져올 수 있는 api작성(php, code ignitor)

---
+++ InnerBeauty/app/src/main/java/com/boostcamp/eunjilee/innerbeauty/fragment/PlayFragment.java
@@ -0,0 +1,74 @@
+package com.boostcamp.eunjilee.innerbeauty.fragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.boostcamp.eunjilee.innerbeauty.DetailContentActivity;
+import com.boostcamp.eunjilee.innerbeauty.R;
+import com.boostcamp.eunjilee.innerbeauty.adapter.ExhibitionAdapter;
+import com.boostcamp.eunjilee.innerbeauty.adapter.PlayAdapter;
+import com.boostcamp.eunjilee.innerbeauty.model.ExhibitionModel;
+import com.boostcamp.eunjilee.innerbeauty.model.PlayModel;
+import com.boostcamp.eunjilee.innerbeauty.module.ExhibitionLoadModule;
+import com.boostcamp.eunjilee.innerbeauty.module.PlayLoadModule;
+import com.boostcamp.eunjilee.innerbeauty.service.ExhibitionService;
+import com.boostcamp.eunjilee.innerbeauty.service.PlayService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
+public class PlayFragment extends Fragment {
+    @BindView(R.id.rv_play)
+    RecyclerView mPlayRecyclerView;
+
+    private List<PlayModel> mPlayList;
+    private PlayAdapter mPlayAdapter;
+    private PlayLoadModule mPlayLoadModule;
+    private int PAGE = 1;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_play, container, false);
+        ButterKnife.bind(this, view);
+
+        initRecyclerView();
+        loadPlayList();
+        return view;
+    }
+
+    private void initRecyclerView() {
+        mPlayList = new ArrayList<PlayModel>();
+        mPlayRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)); // 2 items in a vertical line.
+        mPlayAdapter = new PlayAdapter(getActivity(), mPlayList);
+        mPlayRecyclerView.setAdapter(mPlayAdapter);
+    }
+
+    private void loadPlayList() {
+        mPlayLoadModule = new PlayLoadModule();
+        mPlayLoadModule.getPlayListByAsync(PAGE, new PlayService.getPlayListCallback() {
+            @Override
+            public void success(List<PlayModel> playModelList) {
+                mPlayList.clear();
+                mPlayList.addAll(playModelList);
+                mPlayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void error(Throwable throwable) {
+                Log.v("daisy", "error!!");
+            }
+        });
+    }
+}
---
+++ InnerBeauty/app/src/main/java/com/boostcamp/eunjilee/innerbeauty/model/ExhibitionModel.java
@@ -0,0 +1,211 @@
+package com.boostcamp.eunjilee.innerbeauty.model;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+
+/**
+ * Created by eunjilee on 06/02/2017.
+ */
+
+public class ExhibitionModel implements Serializable {
+    @SerializedName("exhibition_id")
+    int exhibitionId;
+    @SerializedName("exhibition_code")
+    int exhibitionCode;
+    @SerializedName("exhibition_title")
+    String exhibitionTitle;
+    @SerializedName("exhibitiion_artist")
+    String exhibitionArtist;
+    @SerializedName("start_date")
+    String startDate;
+    @SerializedName("end_date")
+    String endDate;
+    @SerializedName("exhibition_place")
+    String exhibitionPlace;
+    @SerializedName("exhibition_address")
+    String exhibitionAddress;
+    @SerializedName("exhibition_picture")
+    String exhibitionPicture;
+    @SerializedName("exhibition_price_adult")
+    int priceAdult;
+    @SerializedName("exhibition_price_student")
+    int priceStudent;
+    @SerializedName("exhibition_price_children")
+    int priceChildren;
+    @SerializedName("exhibition_price_old_infirm")
+    int priceOldInfirm;
+    @SerializedName("play_price_old_infirm")
+    int priceSpecial;
+    @SerializedName("exhibition_call")
+    String exhibitionCall;
+    @SerializedName("exhibition_site")
+    String exhibitionSite;
+
+    int exhibitionclicked;
+    int exhibitionLiked;
+    int exhibitionShared;
+    String exhibitionComments;
+
+
+    public int getExhibitionId() {
+        return exhibitionId;
+    }
+
+    public void setExhibitionId(int exhibitionId) {
+        this.exhibitionId = exhibitionId;
+    }
+
+    public int getExhibitionCode() {
+        return exhibitionCode;
+    }
+
+    public void setExhibitionCode(int exhibitionCode) {
+        this.exhibitionCode = exhibitionCode;
+    }
+
+    public String getExhibitionTitle() {
+        return exhibitionTitle;
+    }
+
+    public void setExhibitionTitle(String exhibitionTitle) {
+        this.exhibitionTitle = exhibitionTitle;
+    }
+
+    public String getExhibitionArtist() {
+        return exhibitionArtist;
+    }
+
+    public void setExhibitionArtist(String exhibitionArtist) {
+        this.exhibitionArtist = exhibitionArtist;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getExhibitionPlace() {
+        return exhibitionPlace;
+    }
+
+    public void setExhibitionPlace(String exhibitionPlace) {
+        this.exhibitionPlace = exhibitionPlace;
+    }
+
+    public String getExhibitionAddress() {
+        return exhibitionAddress;
+    }
+
+    public void setExhibitionAddress(String exhibitionAddress) {
+        this.exhibitionAddress = exhibitionAddress;
+    }
+
+    public String getExhibitionPicture() {
+        return exhibitionPicture;
+    }
+
+    public void setExhibitionPicture(String exhibitionPicture) {
+        this.exhibitionPicture = exhibitionPicture;
+    }
+
+    public int getPriceAdult() {
+        return priceAdult;
+    }
+
+    public void setPriceAdult(int priceAdult) {
+        this.priceAdult = priceAdult;
+    }
+
+    public int getPriceStudent() {
+        return priceStudent;
+    }
+
+    public void setPriceStudent(int priceStudent) {
+        this.priceStudent = priceStudent;
+    }
+
+    public int getPriceChildren() {
+        return priceChildren;
+    }
+
+    public void setPriceChildren(int priceChildren) {
+        this.priceChildren = priceChildren;
+    }
+
+    public int getPriceOldInfirm() {
+        return priceOldInfirm;
+    }
+
+    public void setPriceOldInfirm(int priceOldInfirm) {
+        this.priceOldInfirm = priceOldInfirm;
+    }
+
+    public String getExhibitionCall() {
+        return exhibitionCall;
+    }
+
+    public void setExhibitionCall(String exhibitionCall) {
+        this.exhibitionCall = exhibitionCall;
+    }
+
+    public String getExhibitionSite() {
+        return exhibitionSite;
+    }
+
+    public void setExhibitionSite(String exhibitionSite) {
+        this.exhibitionSite = exhibitionSite;
+    }
+
+    public int getExhibitionclicked() {
+        return exhibitionclicked;
+    }
+
+    public void setExhibitionclicked(int exhibitionclicked) {
+        this.exhibitionclicked = exhibitionclicked;
+    }
+
+    public int getExhibitionLiked() {
+        return exhibitionLiked;
+    }
+
+    public void setExhibitionLiked(int exhibitionLiked) {
+        this.exhibitionLiked = exhibitionLiked;
+    }
+
+    public int getExhibitionShared() {
+        return exhibitionShared;
+    }
+
+    public void setExhibitionShared(int exhibitionShared) {
+        this.exhibitionShared = exhibitionShared;
+    }
+
+    public String getExhibitionComments() {
+        return exhibitionComments;
+    }
+
+    public void setExhibitionComments(String exhibitionComments) {
+        this.exhibitionComments = exhibitionComments;
+    }
+
+    public int getPriceSpecial() {
+        return priceSpecial;
+    }
+
+    public void setPriceSpecial(int priceSpecial) {
+        this.priceSpecial = priceSpecial;
+    }
+}
+
